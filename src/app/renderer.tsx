69cfc0e56c5fa3a787a224da39644af4435f4671
Isidro <isidro.calderon@outlook.com>
2019-10-31 12:06:42 -0400

    initial

---
+++ src/app/renderer.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Dashboard } from "./components/dashboard";
+
+ReactDOM.render(<Dashboard />, document.getElementById("renderer"));
